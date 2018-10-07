193f7ea5a488198ce2e3d9bc215752c85ce15845
yasminath SIDI ALI <sidialiyasminath@gmail.com>
2018-10-07 10:41:09 +0100

    exerciceo.py

---
+++ exercice0.py
@@ -0,0 +1,8 @@
+chaine1='I\'m yasminath, an active member of Ceceonat'
+chaine2='Email: sidialiyasminath@gnail.com'
+chaine3='logins: \nGithub: yasminath \nGitlab: yasminath'
+chaine4='Programming langage: python'
+print(chaine1)
+print(chaine2)
+print(chaine3)
+print(chaine4)
